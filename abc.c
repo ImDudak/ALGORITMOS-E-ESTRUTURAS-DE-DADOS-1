d37f147132921d0a160a250863fc7ac0df8e92e7
ImDudak <80131646+ImDudak@users.noreply.github.com>
2022-12-18 17:21:00 -0300

    Update abc.c

--- abc.c
+++ abc.c
@@ -1,7 +1,7 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <locale.h>
-#include <C:\Users\dudak\Desktop\Programim in C\teste3\abc.h>
+#include <abc.h>
 
 // Declaracao da Estrutura Nodo 
 typedef struct sNodo {
